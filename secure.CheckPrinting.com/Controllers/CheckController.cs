b61f2ffd5982f6be4a9a5abeb86db6a575d1818a
Petchimuthu P <git+petchimuthu@spanenterprises.com>
2018-10-06 18:52:27 +0530

    New View added

---
+++ secure.CheckPrinting.com/Controllers/CheckController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace secure.CheckPrinting.com.Controllers
+{
+    public class CheckController : Controller
+    {
+        #region Index
+        // GET: Check
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Add New Check Templete
+        /// <summary>
+        /// Add New Check Templete
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddNewCheckTemplete(long id)
+        {
+            return View();
+        }
+        #endregion
+
+    }
+}
