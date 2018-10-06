7cfd37a47b2488b7c2432ca26bad35ff110e64ad
Petchimuthu P <git+petchimuthu@spanenterprises.com>
2018-10-06 18:45:45 +0530

    Project added

---
+++ secure.CheckPrinting.com/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace secure.CheckPrinting.com
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
